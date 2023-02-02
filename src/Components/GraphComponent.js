7288edb8d333118bc55e00feab67fa941ccf93f5
mystry22 <alanemehenry6@gmail.com>
2023-02-02 06:54:22 -0800

    hopfule changes

--- src/Components/GraphComponent.js
+++ src/Components/GraphComponent.js
@@ -1,16 +1,17 @@
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import React from 'react';
 import { LineChart } from "react-native-chart-kit";
-
+import { VictoryChart, VictoryBar, VictoryTheme } from 'victory-native';
+import colors from '../Utils/color';
 const GraphComponent = ({ graphData }) => {
-   
+
 
     return (
 
 
         <View>
 
-            {
+            {/* {
                 graphData.length > 0 ? 
                 <View>
                     <Text style={styles.boldText}>Last 5 Transactions</Text>
@@ -50,6 +51,14 @@ const GraphComponent = ({ graphData }) => {
                     borderRadius: 16
                 }}
             />
+
+            <VictoryChart theme={VictoryTheme.grayscale}>
+
+                <VictoryBar animate data={graphData} x="trans_date" y="balance">
+
+                </VictoryBar>
+
+            </VictoryChart>
                 </View>
 
                 : 
@@ -57,9 +66,26 @@ const GraphComponent = ({ graphData }) => {
                 <Text style={{color:'gray', fontSize:18}}>
                     No Data
                 </Text>
+            } */}
+
+            {graphData.length > 0 ?
+
+                graphData.map(tractions => (
+                    <View style={styles.transactions} key={tractions._id} >
+                        <View style={[styles.status, { backgroundColor: tractions.color }]}>
+                        </View>
+
+
+                        <Text style={[styles.bookieCode, { color: colors.gray }]}>{tractions.code}</Text>
+                        <Text style={styles.bookieCode}>{tractions.trans_date}</Text>
+                        <Text style={[styles.bookieCode, { color: colors.gray }]}>{tractions.amount}</Text>
+                    </View>
+                )) :
+                <Text style={{ color: colors.gray }}>No Transaction Available Yet</Text>
+
             }
 
-            
+
 
 
         </View>
@@ -75,4 +101,25 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         color: 'gray'
     },
+    transactions: {
+        backgroundColor: colors.purple,
+        height: 50,
+        borderRadius: 15,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom:20
+    },
+    status: {
+        height: 30,
+        width: 30,
+        borderRadius: 50,
+        borderWidth: 4,
+        borderColor: colors.dark
+    },
+    bookieCode: {
+        fontSize: 18,
+        fontWeight: '700',
+
+    }
 })
