32a355e8896db47a8e577f6a26d39b0f77dc2dcb
mystry22 <alanemehenry6@gmail.com>
2023-02-10 01:37:40 -0800

    revert pie chart

--- src/Components/GraphComponent.js
+++ src/Components/GraphComponent.js
@@ -1,47 +1,90 @@
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import React from 'react';
-import PieChart from 'react-native-pie-chart';
+import { LineChart } from "react-native-chart-kit";
+import { VictoryChart, VictoryBar, VictoryTheme } from 'victory-native';
 import colors from '../Utils/color';
-const GraphComponent = ({ series, color }) => {
-    const widthAndHeight = 250;
+const GraphComponent = ({ graphData }) => {
+
 
     return (
 
 
         <View>
 
-           
+            {/* {
+                graphData.length > 0 ? 
+                <View>
+                    <Text style={styles.boldText}>Last 5 Transactions</Text>
+            <LineChart
+                data={{
+                    labels: graphData.map(trans => {return trans.trans_date.substr(0,6) }),
+                    datasets: [
+                        {
+                            data: graphData.map(trans => {return trans.balance })
+                        }
+                    ]
+                }}
+                width={Dimensions.get("window").width - 20} // from react-native
+                height={300}
+                yAxisLabel="N"
+                yAxisSuffix=""
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                    backgroundColor: "#e26a00",
+                    backgroundGradientFrom: "#fb8c00",
+                    backgroundGradientTo: "green",
+                    decimalPlaces: 2, // optional, defaults to 2dp
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    style: {
+                        borderRadius: 16
+                    },
+                    propsForDots: {
+                        r: "6",
+                        strokeWidth: "2",
+                        stroke: "#ffa726"
+                    }
+                }}
+                bezier
+                style={{
+                    marginVertical: 8,
+                    borderRadius: 16
+                }}
+            />
 
-            <Text style={styles.boldText}>Visualization</Text>
+            <VictoryChart theme={VictoryTheme.grayscale}>
 
-            {series.length > 0 ?
+                <VictoryBar animate data={graphData} x="trans_date" y="balance">
 
-            <View style={styles.div}>
+                </VictoryBar>
 
-                <PieChart
-                widthAndHeight={widthAndHeight}
-                series={series}
-                sliceColor={color}
-              />
-
-              <View>
-                <Text style={{color:'#fff',fontSize:18}}>Legend</Text>
-
-                <View style={styles.div}>
-                    <View style={[styles.indicators,{backgroundColor:'orange'}]}></View> 
-                    <Text style={{color:'gray', lineHeight:13}}>% {series[1]} loss </Text>
+            </VictoryChart>
                 </View>
-                <View style={styles.div}>
-                    <View style={[styles.indicators,{backgroundColor:'green'}]}></View> 
-                    <Text style={{color:'gray', lineHeight:13}}>% {series[1]} Win </Text>
-                </View>
-                <View style={styles.div}>
-                    <View style={[styles.indicators,{backgroundColor:'white'}]}></View> 
-                    <Text style={{color:'gray', lineHeight:13}}>% {series[2]} pendings </Text>
-                </View>
-              </View>
-            </View>
-                :
+
+                : 
+
+                <Text style={{color:'gray', fontSize:18}}>
+                    No Data
+                </Text>
+            } */}
+
+            <Text style={styles.boldText}>Last 5 Transactions</Text>
+
+            {graphData.length > 0 ?
+
+               
+
+                graphData.map(tractions => (
+                    <View style={styles.transactions} key={tractions._id} >
+                        <View style={[styles.status, { backgroundColor: tractions.color }]}>
+                        </View>
+
+
+                        <Text style={[styles.bookieCode, { color: colors.gray }]}>{tractions.code}</Text>
+                        <Text style={styles.bookieCode}>{tractions.trans_date}</Text>
+                        <Text style={[styles.bookieCode, { color: colors.gray }]}>{tractions.amount}</Text>
+                    </View>
+                )) :
                 <Text style={{ color: colors.gray }}>No Transaction Available Yet</Text>
 
             }
@@ -82,14 +125,5 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: '700',
 
-    },
-    div:{
-        flexDirection:'row',
-        marginBottom:5
-    },
-    indicators:{
-        width:50,
-        height:10
-    },
-    
+    }
 })
